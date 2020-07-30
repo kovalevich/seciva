5b13650955f7bc27806f50293a47c4382c5308d0
Kovalevich <evgen.kovalevich@gmail.com>
2020-07-30 12:12:12 +0300

    Первый рабочий результат

--- classes/seciva.py
+++ classes/seciva.py
@@ -1,67 +1,73 @@
-from typing import overload, List
+from numpy import random, zeros, array, dot, exp
 
 
-class Neurone:
-    """Класс нейрона"""
-
-    def __init__(self, value: float = 0):
-        self.__value = value
-        self.__error = 0
-        pass
-
-    def value(self, value: float = None) -> float:
-        """Реализация сеттера и геттера для поля __value"""
-        if value is not None:
-            self.__value = value
-
-        return self.__value
-
-    def error(self, value: float = None) -> float:
-        """Реализация сеттера и геттера для поля __error"""
-        if value is not None:
-            self.__error = value
-
-        return self.__error
-
-    def __call__(self, *args, **kwargs) -> float:
-        value = kwargs.get('value', None)
-        return self.value(value)
+def sigmoid(x):
+    """ Функция ктивации нейрона """
+    return 1 / (1 + exp(-x))
 
 
-class Layer:
-
-    @overload
-    def __init__(self, count_neurones: int): pass
-
-    @overload
-    def __init__(self, neurones_values: List[float]): pass
-
-    def __init__(self, *args, **kwargs):
-        """Конструктор слоя может принимать целое число - количество нейронов в слое,
-        либо список значений нейронов. Если парамоетров нет, создается слой без нейронов"""
-        neurones = []
-        for arg in args:
-            if isinstance(arg, int):
-                neurones = [Neurone(x) for x in range(arg)]
-            if isinstance(arg, List):
-                neurones = [Neurone(x) for x in arg]
-        self.__neurones = neurones
-
-    def get_list_view(self) -> list:
-        """Метод возвращает список значений нейронов сети"""
-        return [neurone() for neurone in self.__neurones]
+def sigmoid_derivative(x):
+    """ Функция деактивации """
+    return x * (1 - x)
 
 
-class Seciva:
-    """Класс нейросети"""
+class Nn:
 
-    def __init__(self, size_in_layer: int, size_out_layer: int):
-        self.__in: Layer = Layer(size_in_layer)
-        self.__out: Layer = Layer(size_out_layer)
+    """ Класс нейронной сети реализует логику прямого прохождения сигнала по сети
+    и обратное распространение ошибки с корректировкой весов связей"""
 
-    def solution(self, input_data: list) -> list:
-        """Метод возвращает решение нейронной сети для input_data"""
-        return self.__out.get_list_view()
+    # Конструктор принимает параметры нейронной сети - layers
+    # передаем список, длина которого = количество слоев,
+    # а каждое значение = количество нейронов в соответствующем слое
+    def __init__(self, layers: list):
+        self.__layers = [zeros(layer) for layer in layers]
+        self.__errors = [zeros(layer) for layer in layers]
+        self.__weights = []
+        self.__generate_weights()
+        self.__size = len(self.__layers)
 
-    def train(self, input_data, output_data, iterations: int = 1000):
-        pass
+    def solution(self, input_data: list):
+        """ Метод для решения (прямого распространения) входных данных input_data """
+        self.__in(data=input_data)
+        for i in range(self.__size - 1):
+            next_neurones = dot(self.__layers[i], self.__weights[i])
+            self.__layers[i+1] = sigmoid(next_neurones)
+        # возвращаем значения нейронов выходного слоя
+        return self.__out()
+
+    def train(self, input_data: list, output_data: list, iterations: int = 1000):
+        """ Метод обучения по входным и выходным данным """
+        while iterations > 0:
+            solution = self.solution(input_data)
+            error = output_data - solution
+            self.__back(error)
+            iterations -= 1
+
+    def __back(self, error: list):
+        """ Метод обратного распространения ошибки """
+        self.__errors[self.__size - 1] = error
+        for i in range(self.__size-2, -1, -1):
+            e = dot(self.__errors[i+1], self.__weights[i].T)
+            self.__errors[i] = e
+
+        for i in range(self.__size - 1):
+            delta = dot(self.__layers[i].T, self.__errors[i+1] * sigmoid_derivative(self.__layers[i+1]))
+            self.__weights[i] += delta
+
+    def __generate_weights(self):
+        """ Метод генерирует матрицы весов """
+        for i, layer in enumerate(self.__layers[:-1]):
+            self.__weights.append(random.random((len(layer), len(self.__layers[i + 1]))))
+
+    def __in(self, data: list = None):
+        return self.__layer(data=data, index=0)
+
+    def __out(self, data: list = None):
+        return self.__layer(data=data, index=(len(self.__layers) - 1))
+
+    def __layer(self, data: list = None, index: int = 0):
+        """ Метод реализует сеттер и геттер для self.__layers """
+        if data is not None:
+            self.__layers[index] = array(data)
+        return self.__layers[index]
+
